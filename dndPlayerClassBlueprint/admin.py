0fb500884cf7284a6f11ccbd162dacbd8bc8e8c0
Amit Amanna <amit.natheniel@gmail.com>
2021-10-04 11:19:57 +0200

    added spellcasting to admin.py

--- dndPlayerClassBlueprint/admin.py
+++ dndPlayerClassBlueprint/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import PlayerClass
 from .player_class_models.magic import Magic
+from .player_class_models.spellcasting import Spellcasting
 
 admin.site.register(PlayerClass)
 admin.site.register(Magic)
+admin.site.register(Spellcasting)
