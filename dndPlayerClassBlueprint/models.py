0e54f181b575e0bfa674484bbdd9b401d774bc85
Amit Amanna <amit.natheniel@gmail.com>
2021-10-04 16:20:58 +0200

    renamed features and made it nullable

--- dndPlayerClassBlueprint/models.py
+++ dndPlayerClassBlueprint/models.py
@@ -6,13 +6,12 @@ from dndPlayerClassBlueprint.player_class_models import languages, features, spe
 
 class PlayerClass(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid1, editable=False)
-    # TODO UserID as FK
     name = models.CharField(max_length=20)
     description = models.TextField()
     hit_die_sides = models.PositiveIntegerField()
     proficiencies_id = models.ForeignKey("proficiencies", on_delete=models.SET_NULL, null=True)
     languagesID = models.ForeignKey("languages", on_delete=models.SET_NULL, null=True)
-    features_junction = models.ManyToManyField('features')
+    features = models.ManyToManyField('features', null=True, name='features_junction')
     spellcastingID = models.ForeignKey("spellcasting", on_delete=models.SET_NULL, null=True)
     # TODO LevellingID
 
