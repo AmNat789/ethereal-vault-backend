539e55cf3bbef75064dc5e3a08e51c16f996d1c8
Amit Amanna <amit.natheniel@gmail.com>
2021-10-04 11:19:40 +0200

    changed spell slots array size

--- dndPlayerClassBlueprint/player_class_models/spellcasting.py
+++ dndPlayerClassBlueprint/player_class_models/spellcasting.py
@@ -9,10 +9,10 @@ from dndPlayerClassBlueprint.player_class_models import magic
 class Spellcasting(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     spellcasting_attribute = models.CharField(max_length=255, choices=Attribute.get_tuple(), null=True)
-    languagesID = models.ForeignKey("magic", on_delete=models.SET_NULL, null=True)
+    given_magic = models.ForeignKey("magic", on_delete=models.SET_NULL, null=True)
     additional_spells = models.PositiveIntegerField()
     additional_cantrips = models.PositiveIntegerField()
-    spell_slots = ArrayField(models.PositiveIntegerField(), size=1)
+    spell_slots = ArrayField(models.PositiveIntegerField(), size=9)
     spells_prepared = models.PositiveIntegerField(null=True)
 
 
